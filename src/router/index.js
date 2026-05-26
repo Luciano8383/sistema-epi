ee3575b409e3d9ad4f0a0c2c91ec04cc94438a05
Luciano8383 <luciano_2115@hotmail.com>
2026-05-26 20:51:00 -0300

    OITAVO COMMIT# Please enter the commit message for your changes. Lines starting

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Cadastrar_funcionario from '../views/Cadastrar_funcionario.vue'
 
-import Login from '../views/login.vue'
-import Inicio from '../views/inicio.vue'
+import Login from '../views/Login.vue'
+import Inicio from '../views/Inicio.vue'
 import Cadastro from '../views/Cadastro.vue'
-import Estoque from '../views/estoque.vue'
-import Entrega_epi from '../views/entrega_epi.vue'
+import Estoque from '../views/Estoque.vue'
+import Entrega_epi from '../views/Entrega_epi.vue'
 
 import { useSupabase } from '../composables/useSupabase'
 
@@ -17,15 +17,15 @@ const routes = [
 
 {path: '/Login', component: Login},
 
-{path: '/inicio', component: Inicio,
+{path: '/Inicio', component: Inicio,
   
 
 children: [
 { path: '/cadastrar_funcionario', component: Cadastrar_funcionario},
 
 {path: '/Cadastro', component: Cadastro},
-{path: '/estoque', component: Estoque},
-{path: '/entrega_epi', component: Entrega_epi},
+{path: '/Estoque', component: Estoque},
+{path: '/Entrega_epi', component: Entrega_epi},
 
 ] 
 }
